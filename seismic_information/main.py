5b1260bf74aeee1b81b758bce911fc27bb5bd257
Yu Lin <linyuwh@gmail.com>
2020-09-17 10:49:40 +0800

    seismic information, crawed from gov website

---
+++ seismic_information/main.py
@@ -0,0 +1,37 @@
+import requests
+import os, time
+
+max_record = 438
+record_range = list((range(1, max_record, 45))) + [max_record+2]
+
+headers = {'Connection': 'keep-alive', 
+   'Accept': 'application/xml, text/xml, */*; q=0.01', 
+   'X-Requested-With': 'XMLHttpRequest', 
+   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36', 
+   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+   'Origin': 'http://www.js-seism.gov.cn', 
+   'Referer':'http://www.js-seism.gov.cn/col/col981/index.html?uid=926&pageNum=1', 
+   'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7', 
+   'Cookie': 'JSESSIONID=3C2BF5F1EAEDCE181F1266AF9E6AD5FE'} 
+
+data = {'col': '1',
+ 'appid': '1',
+ 'webid': '1',
+ 'path': '%2F',
+ 'columnid': '981',
+ 'sourceContentType': '1',
+ 'unitid': '926',
+ 'webname': '%E6%B1%9F%E8%8B%8F%E9%98%B2%E9%9C%87%E5%87%8F%E7%81%BE',
+ 'permissiontype': '0'
+ }
+
+request_url = 'http://www.js-seism.gov.cn/module/web/jpage/dataproxy.jsp?startrecord={}&endrecord={}&perpage=15'
+
+store_dire = 'new_web_site'
+for idx, num in enumerate(record_range[:-1]):
+    r = requests.post(request_url.format(num, record_range[idx+1]-1), headers=headers, data=data)
+    # translated = r.text
+    translated = r.text.encode('utf-8')
+    with open(os.path.join(store_dire, f'new_{idx+1}_page.txt'), 'wb') as fd:
+        fd.write(translated)
+    time.sleep(1)
---
+++ seismic_information/reg_test.py
@@ -0,0 +1,21 @@
+import os, re
+
+store_dir = 'new_web_site'
+prog = re.compile(r"据江苏省地震台网测定：(\d+)年(\d+)月(\d+)日(\d+)时(\d+)分，在(.+)[\(（]北纬([0-9.]+)度[,，]东经([0-9.]+)度[\)）]发生(M?[0-9.]+)级地震")
+templete = "{}年{}月{}日{}时{}分, {}(北纬{}度, 东经{}度)发生{}级别地震。"
+# prog = re.compile(r"据江苏省地震台网测定：(\d+)年(\d+)月(\d+)日(\d+)时(\d+)分，在(.+)\'>")
+# templete = "{}年{}月{}日{}时{}分, {}。"
+
+final_result = ""
+# 据江苏省地震台网测定：2020年06月27日19时09分，在江苏盐城市射阳县
+
+for idx in range(1, 11):
+    with open(os.path.join(store_dir, f'new_{idx}_page.txt'), 'r', encoding="utf-8") as fd:
+        total_text = fd.read()
+        for line in total_text.split('</a></td>'):
+            res = prog.search(line)
+            if res:
+                final_result += templete.format(*[res.group(ii) for ii in range(1, 10)]) + '\n'
+    
+with open('final_result.txt', 'wb') as fd:
+    fd.write(final_result.encode('utf-8'))
